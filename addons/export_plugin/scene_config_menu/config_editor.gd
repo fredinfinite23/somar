4dd5585ffff5c2ccd1c68cdcf0a1bd679177580c
Daniel Castellanos <decacis@gmail.com>
2024-11-07 20:04:21 -0600

    Improvements to config editor

--- addons/export_plugin/scene_config_menu/config_editor.gd
+++ addons/export_plugin/scene_config_menu/config_editor.gd
@@ -5,6 +5,7 @@ extends Panel
 @onready var initial_config_file_dialog : FileDialog = %InitialConfigFileDialog
 @onready var initial_config_btn : Button = %InitialConfigBtn
 @onready var main_container : MarginContainer = %MainContainer
+@onready var sync_confirm_dialog : ConfirmationDialog = %SyncConfirmDialog
 
 @onready var save_btn : Button = %SaveBtn
 
@@ -108,6 +109,24 @@ var scenes_config : ConfigFile
 var ocean_config : Dictionary
 var shore_config : Dictionary
 
+var last_recorded_save_time : float = 0
+var out_of_sync_timestamps : Array[String] = []
+
+
+func _notification(what: int) -> void:
+	if what == NOTIFICATION_APPLICATION_FOCUS_IN:
+		if scenes_config:
+			var last_save_time : float = FileAccess.get_modified_time(_get_scenes_config_save_path())
+
+			if last_save_time > 0 \
+			and last_recorded_save_time > 0 \
+			and abs(last_recorded_save_time - last_save_time) > 1 \
+			and not out_of_sync_timestamps.has(str(last_save_time)):
+				if not _verify_sync():
+					out_of_sync_timestamps.push_back(str(last_save_time))
+					sync_confirm_dialog.dialog_text = "The config file %s is not in sync with the current editor data. What would you like to do?\n\nNote: you have to manually click the \"Save\" button to save the editor data if you wish to keep it.\n" % _get_scenes_config_save_path()
+					sync_confirm_dialog.show()
+
 
 func _ready() -> void:
 	if UTIL.is_in_edited_scene(self):
@@ -115,6 +134,9 @@ func _ready() -> void:
 
 	if not save_btn.pressed.is_connected(_save_data):
 		save_btn.pressed.connect(_save_data)
+	
+	if not sync_confirm_dialog.canceled.is_connected(_handle_sync_dialog_canceled):
+		sync_confirm_dialog.canceled.connect(_handle_sync_dialog_canceled)
 
 	save_data = ConfigFile.new()
 	var save_data_err : Error = save_data.load(SAVE_DATA_PATH)
@@ -137,6 +159,21 @@ func _ready() -> void:
 			initial_config_btn.disabled = false
 
 
+func _handle_sync_dialog_canceled() -> void:
+	var err : Error = scenes_config.load(_get_scenes_config_save_path())
+	
+	if err == OK:
+		last_recorded_save_time = FileAccess.get_modified_time(_get_scenes_config_save_path())
+
+		ocean_config = scenes_config.get_value("ocean", "config", DEFAULT_OCEAN_DICT.duplicate())
+		shore_config = scenes_config.get_value("shore", "config", DEFAULT_SHORE_DICT.duplicate())
+
+		_process_saved_data()
+	
+	else:
+		print_debug("ERROR: unable to sync editor with current save config file!")
+
+
 func _show_initial_config_dir_selection_dialog() -> void:
 	initial_config_file_dialog.show()
 
@@ -147,21 +184,26 @@ func _handle_initial_config_dir_selected(dir : String) -> void:
 	scenes_config.set_value("ocean", "config", DEFAULT_OCEAN_DICT.duplicate())
 	scenes_config.set_value("shore", "config", DEFAULT_SHORE_DICT.duplicate())
 	
-	scenes_config.save(_get_scenes_config_save_path())
+	var save_err : Error = scenes_config.save(_get_scenes_config_save_path())
+	if save_err == OK:
+		ocean_config = scenes_config.get_value("ocean", "config", DEFAULT_OCEAN_DICT.duplicate())
+		ocean_config = scenes_config.get_value("shore", "config", DEFAULT_SHORE_DICT.duplicate())
 
-	ocean_config = scenes_config.get_value("ocean", "config", DEFAULT_OCEAN_DICT.duplicate())
-	ocean_config = scenes_config.get_value("shore", "config", DEFAULT_SHORE_DICT.duplicate())
-
-	initial_config_container.visible = false
+		initial_config_container.visible = false
+		
+		_initialize_main_editor_scene(false)
+		print("Initial Config Saved!")
 	
-	_initialize_main_editor_scene(false)
-	print("Initial Config Saved!")
+	else:
+		print_debug("ERROR: unable to save config data! (%s)" % save_err)
 
 
 func _initialize_main_editor_scene(load_plugin_config : bool) -> void:
 	if load_plugin_config:
 		scenes_config = ConfigFile.new()
 		scenes_config.load(_get_scenes_config_save_path())
+	
+	last_recorded_save_time = FileAccess.get_modified_time(_get_scenes_config_save_path())
 
 	ocean_config = scenes_config.get_value("ocean", "config", DEFAULT_OCEAN_DICT.duplicate())
 	shore_config = scenes_config.get_value("shore", "config", DEFAULT_SHORE_DICT.duplicate())
@@ -245,6 +287,18 @@ func _handle_tree_item_selected(scene_type : SceneManager.PlayerContext) -> void
 			default_container.visible = true
 
 
+func _add_bottlenose_dolphin_item(scene_type : SceneManager.PlayerContext) -> void:
+	var bottlenose_dolphin_item : BottlenoseConfigItem = BOTTLENOSE_DOLPHIN_ITEM.instantiate()
+
+	# TODO: Add shore here
+	if scene_type == SceneManager.PlayerContext.OCEAN:
+		ocean_bottlenose_dolphin_items_container.add_child(bottlenose_dolphin_item)
+		bottlenose_dolphin_item.scene_type = scene_type
+	else:
+		shore_bottlenose_dolphin_items_container.add_child(bottlenose_dolphin_item)
+		bottlenose_dolphin_item.scene_type = scene_type
+
+
 func _get_scenes_config_save_path() -> String:
 	return "%s%s" % [
 			save_data.get_value("scene_config_data", "save_path", "res://"),
@@ -254,6 +308,9 @@ func _get_scenes_config_save_path() -> String:
 
 func _process_saved_data() -> void:
 	# OCEAN
+	for current_ocean_bottlenose_item : Node in ocean_bottlenose_dolphin_items_container.get_children():
+		current_ocean_bottlenose_item.queue_free()
+
 	for bottlenose_dolphin_def : Dictionary in ocean_config.animals.dolphins.bottlenose:
 		var bottlenose_dolphin_item : BottlenoseConfigItem = BOTTLENOSE_DOLPHIN_ITEM.instantiate()
 		ocean_bottlenose_dolphin_items_container.add_child(bottlenose_dolphin_item)
@@ -265,6 +322,9 @@ func _process_saved_data() -> void:
 	)
 
 	# SHORE
+	for current_shore_bottlenose_item : Node in shore_bottlenose_dolphin_items_container.get_children():
+		current_shore_bottlenose_item.queue_free()
+	
 	for bottlenose_dolphin_def : Dictionary in shore_config.animals.dolphins.bottlenose:
 		var bottlenose_dolphin_item : BottlenoseConfigItem = BOTTLENOSE_DOLPHIN_ITEM.instantiate()
 		shore_bottlenose_dolphin_items_container.add_child(bottlenose_dolphin_item)
@@ -278,40 +338,77 @@ func _process_saved_data() -> void:
 func _save_data() -> void:
 	# OCEAN
 	# Update ocean_config
-	var new_bottlenose_array : Array = []
-	for bottlenose_dolphin_item : BottlenoseConfigItem in ocean_bottlenose_dolphin_items_container.get_children():
-		new_bottlenose_array.push_back(bottlenose_dolphin_item.get_data())
-	
-	ocean_config.animals.dolphins.bottlenose = new_bottlenose_array
-
-	# Update boats info
-	ocean_config.boats.inflatable_patrol = ocean_inflatable_patrol_boat_item.get_data()
-
-	scenes_config.set_value("ocean", "config", ocean_config)
+	var current_ocean_config_data : Dictionary = _get_current_config_data(SceneManager.PlayerContext.OCEAN)
+	scenes_config.set_value("ocean", "config", current_ocean_config_data)
 
 	# SHORE
 	# Update shore_config
-	var new_bottlenose_shore_array : Array = []
-	for bottlenose_dolphin_item : BottlenoseConfigItem in shore_bottlenose_dolphin_items_container.get_children():
-		new_bottlenose_shore_array.push_back(bottlenose_dolphin_item.get_data())
+	var current_shore_config_data : Dictionary = _get_current_config_data(SceneManager.PlayerContext.SHORE)
+	scenes_config.set_value("shore", "config", current_shore_config_data)
+
+	var save_err : Error = scenes_config.save(_get_scenes_config_save_path())
+	if save_err == OK:
+		last_recorded_save_time = FileAccess.get_modified_time(_get_scenes_config_save_path())
+	else:
+		print_debug("ERROR: unable to save config data! (%s)" % save_err)
+
+
+func _get_current_config_data(type : SceneManager.PlayerContext) -> Dictionary:
+	var bottlenose_dolphin_items_container : VBoxContainer = ocean_bottlenose_dolphin_items_container
+	var inflatable_patrol_boat_item : InflatablePatrolConfigItem = ocean_inflatable_patrol_boat_item
+
+	if type == SceneManager.PlayerContext.SHORE:
+		bottlenose_dolphin_items_container = shore_bottlenose_dolphin_items_container
+		inflatable_patrol_boat_item = shore_inflatable_patrol_boat_item
 	
-	shore_config.animals.dolphins.bottlenose = new_bottlenose_shore_array
+	# TODO: when general data is implemented, remove these defaults
+	var return_data : Dictionary = {
+		"general": {
+			"min_boat_event_spawn_delay": 60.0,
+			"max_boat_event_spawn_delay": 90.0,
+			"min_after_boat_wildlife_return_time": 10.0,
+			"max_after_boat_wildlife_return_time": 15.0,
+			"new_cycle_delay": 10.0
+		},
+		"animals": {
+			"dolphins": {
+				"bottlenose": []
+			}
+		},
+		"boats": {
+			"inflatable_patrol": {}
+		}
+	}
+
+	var new_bottlenose_array : Array = []
+	for bottlenose_dolphin_item : BottlenoseConfigItem in bottlenose_dolphin_items_container.get_children():
+		new_bottlenose_array.push_back(bottlenose_dolphin_item.get_data())
+	
+	return_data.animals.dolphins.bottlenose = new_bottlenose_array
 
 	# Update boats info
-	shore_config.boats.inflatable_patrol = shore_inflatable_patrol_boat_item.get_data()
+	return_data.boats.inflatable_patrol = inflatable_patrol_boat_item.get_data()
 
-	scenes_config.set_value("shore", "config", shore_config)
-
-	scenes_config.save(_get_scenes_config_save_path())
+	return return_data
 
 
-func _add_bottlenose_dolphin_item(scene_type : SceneManager.PlayerContext) -> void:
-	var bottlenose_dolphin_item : BottlenoseConfigItem = BOTTLENOSE_DOLPHIN_ITEM.instantiate()
+func _verify_sync() -> bool:
+	var editor_ocean_data_dict : Dictionary = _get_current_config_data(SceneManager.PlayerContext.OCEAN)
+	var editor_shore_data_dict : Dictionary = _get_current_config_data(SceneManager.PlayerContext.SHORE)
+	var editor_data : Dictionary = {
+		"ocean": editor_ocean_data_dict,
+		"shore": editor_shore_data_dict
+	}
 
-	# TODO: Add shore here
-	if scene_type == SceneManager.PlayerContext.OCEAN:
-		ocean_bottlenose_dolphin_items_container.add_child(bottlenose_dolphin_item)
-		bottlenose_dolphin_item.scene_type = scene_type
-	else:
-		shore_bottlenose_dolphin_items_container.add_child(bottlenose_dolphin_item)
-		bottlenose_dolphin_item.scene_type = scene_type
+	var saved_config_file : ConfigFile = ConfigFile.new()
+	saved_config_file.load(_get_scenes_config_save_path())
+	var saved_ocean_data_dict : Dictionary = saved_config_file.get_value("ocean", "config", {})
+	var saved_shore_data_dict : Dictionary = saved_config_file.get_value("shore", "config", {})
+	var saved_data : Dictionary = {
+		"ocean": saved_ocean_data_dict,
+		"shore": saved_shore_data_dict
+	}
+
+	saved_config_file = null
+
+	return editor_data.recursive_equal(saved_data, 10)
